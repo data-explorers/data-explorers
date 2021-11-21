7eafcba8eee9dfe9ec5ab1fef43acb7c85d5ba26
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-21 05:49:58 -0600

    add static about site page

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<h1 class="text-4xl text-center m-8 font-extrabold">About</h1>
+
+<main class="prose">
+  <p>
+    Cillum et ex esse proident adipisicing magna reprehenderit minim. Enim proident et culpa ea
+    dolore veniam dolor sint fugiat. Reprehenderit fugiat consectetur qui magna esse mollit elit.
+  </p>
+
+  <p>
+    In est consequat non commodo ipsum in exercitation ut eiusmod id magna ut officia. Quis velit
+    anim fugiat ad. Laborum excepteur aliquip fugiat quis magna occaecat. Nulla et nulla incididunt
+    tempor sit pariatur laboris cupidatat tempor excepteur ipsum nulla sit. Magna minim aliqua ipsum
+    veniam ullamco non aute nostrud qui irure.
+  </p>
+</main>
