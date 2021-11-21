02f0cb3c6186bd954916d573621ea0015fec5b22
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-21 05:55:19 -0600

    add projects show page

---
+++ src/components/project_header.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let project;
+  export let user;
+
+  let tabs = project.tabs.map((tab, index) => {
+    return {
+      label: tab.label,
+      value: index,
+      slug: `/users/${user.username}/${project.slug}/${tab.link}`
+    };
+  });
+</script>
+
+<div class="hero" style="background-image: url(&quot;{project.image}/1600/500&quot;);">
+  <div class="hero-overlay bg-opacity-50" />
+  <div class="text-center hero-content text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-4xl font-bold">{project.title}</h1>
+      <p class="mb-5">{project.summary}</p>
+    </div>
+  </div>
+</div>
+
+<div class="tabs">
+  {#each tabs as tab}
+    <a class="tab tab-bordered" href={tab.slug}>{tab.label}</a>
+  {/each}
+</div>
---
+++ src/routes/users/[users]/[projects].svelte
@@ -0,0 +1,41 @@
+<script context="module">
+  import data from '$lib/data/data.json';
+
+  export async function load({ page }) {
+    let user = data.filter((user) => user.username == page.params.users)[0];
+    let project = user.projects.filter((project) => project.slug == page.params.projects)[0];
+
+    return { props: { project, user } };
+  }
+</script>
+
+<script>
+  import ProjectHeader from '../../../components/project_header.svelte';
+  export let project;
+  export let user;
+</script>
+
+<ProjectHeader {project} {user} />
+
+<main class="">
+  <div class="grid lg:grid-cols-3 md:grid-cols-2 justify-center gap-3">
+    <div class="col-span-2 prose">
+      <dl>
+        <dt class="font-bold">Location</dt>
+        <dd>{project.location}</dd>
+        <dt class="font-bold">Start Date</dt>
+        <dd>{project.start_date}</dd>
+        <dt class="font-bold">Audience</dt>
+        <dd>{project.target_audience}</dd>
+        <dt class="font-bold">Links</dt>
+        {#each project.links as link}
+          <dd><a href={link.url}>{link.text}</a></dd>
+        {/each}
+      </dl>
+      {@html project.description}
+    </div>
+    <div>
+      <img src="/{project.slug}/observations_map_square.jpg" alt="map of {project.title}" />
+    </div>
+  </div>
+</main>
