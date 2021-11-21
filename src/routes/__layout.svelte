1517110129c81f4fdad5a255cb919e0f85084da6
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-21 05:48:04 -0600

    add navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,5 +3,15 @@
 </script>
 
 <main class="container mx-auto">
+  <div class="navbar shadow-lg bg-neutral text-neutral-content">
+    <div class="flex-none px-2 mx-2">
+      <a class="text-lg font-bold" href="/"> Data Explorers </a>
+    </div>
+    <div class="flex-1 px-2 mx-2">
+      <div class="items-stretch hidden lg:flex">
+        <a class="btn btn-ghost btn-sm rounded-btn" href="/about"> About </a>
+      </div>
+    </div>
+  </div>
   <slot />
 </main>
