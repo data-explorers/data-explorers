23ea07024488013730b9c6147fdee7ab6549998e
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-21 05:53:31 -0600

    add users show page

---
+++ src/components/user_header.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let user;
+</script>
+
+<div class="hero" style="background-image: url(&quot;{user.image}/1600/500&quot;);">
+  <div class="hero-overlay bg-opacity-50" />
+  <div class="text-center hero-content text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-4xl font-bold">{user.card_title}</h1>
+      <p class="mb-5">{user.card_summary}</p>
+    </div>
+  </div>
+</div>
---
+++ src/routes/users/[users].svelte
@@ -0,0 +1,36 @@
+<script context="module">
+  import data from '$lib/data/data.json';
+
+  export async function load({ page }) {
+    let user = data.filter((user) => user.username == page.params.users)[0];
+    let projects = user.projects;
+
+    return { props: { user, projects } };
+  }
+</script>
+
+<script>
+  import UserHeader from '../../components/user_header.svelte';
+  export let projects;
+  export let user;
+</script>
+
+<UserHeader {user} />
+
+<h1 class="text-4xl text-center m-8 font-extrabold">Projects!</h1>
+
+<div class="grid lg:grid-cols-3 md:grid-cols-2  justify-center gap-3">
+  {#each projects as project}
+    <div class="card bordered">
+      <figure>
+        <img src="{project.image}/400/250" alt="Photo for {project.title}" />
+      </figure>
+      <a href="/users/{user.username}/{project.slug}">
+        <div class="card-body prose prose-lg">
+          <h2 class="card-title">{project.title}</h2>
+          <p>{project.summary}</p>
+        </div>
+      </a>
+    </div>
+  {/each}
+</div>
