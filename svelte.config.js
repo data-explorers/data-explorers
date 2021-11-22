920d0f9445b8eaba010c63ce60854d2ebe79067c
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-22 15:15:46 -0600

    config static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
+    target: '#svelte',
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    })
   },
 
   preprocess: [
