4509bb3de35cf364c2806b81ea1177e741bfe2e0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-27 03:27:37 -0600

    make entire taxa grid card clickable

--- src/lib/components/taxa_grid.svelte
+++ src/lib/components/taxa_grid.svelte
@@ -22,16 +22,16 @@
 <div class="grid lg:grid-cols-4 md:grid-cols-3  sm:grid-cols-2 justify-center gap-3">
   {#each taxaDisplay as { count, image_url, taxon_id, scientific_name, common_name }}
     <div class="card bordered">
-      <figure>
-        <img src={image_url} alt="photo of {common_name}" />
-      </figure>
+      <a href="/users/{user.username}/{project.slug}/taxa/{taxon_id}">
+        <figure>
+          <img src={image_url} alt="photo of {common_name}" />
+        </figure>
+      </a>
       <div class="card-body">
         <a href="/users/{user.username}/{project.slug}/taxa/{taxon_id}">
-          <h3>{common_name}</h3>
-          <p>
-            <em>{scientific_name}</em><br />
-            {count} observations
-          </p>
+          {#if common_name}<div class="text-lg font-medium">{common_name}</div>{/if}
+          {#if scientific_name}<div class="text-gray-400">({scientific_name})</div>{/if}
+          <div>{count} observations</div>
         </a>
       </div>
     </div>
@@ -40,3 +40,23 @@
 <div class="grid justify-items-center mt-8">
   <button class="btn" on:click={loadMore}>Load More</button>
 </div>
+
+<style>
+  img {
+    height: 225px;
+    object-fit: cover;
+  }
+  @media (max-width: 640px) {
+    img {
+      height: 400px;
+      object-fit: cover;
+    }
+  }
+
+  .card.bordered {
+    border-radius: 0;
+  }
+  .card-body {
+    padding: 1rem;
+  }
+</style>
