97eadea67ab29d29e047ac17edc58829f0c4c590
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-27 03:29:51 -0600

    update styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,8 @@
   import '../app.css';
 </script>
 
-<main class="container mx-auto">
-  <div class="navbar shadow-lg bg-neutral text-neutral-content">
+<div class="container mx-auto">
+  <nav class="navbar shadow-lg bg-neutral text-neutral-content">
     <div class="flex-none px-2 mx-2">
       <a class="text-lg font-bold" href="/"> Data Explorers </a>
     </div>
@@ -12,10 +12,11 @@
         <a class="btn btn-ghost btn-sm rounded-btn" href="/about"> About </a>
       </div>
     </div>
-  </div>
-  <slot />
+  </nav>
+
+  <main><slot /></main>
 
   <footer class="mt-8 p-6 footer bg-neutral text-neutral-content">
     <p>Copyright © 2021</p>
   </footer>
-</main>
+</div>
