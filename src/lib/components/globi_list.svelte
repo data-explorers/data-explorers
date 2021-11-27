c28da9251cbc40bce38040558d471c81aff8efb2
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-27 03:29:31 -0600

    add iNat link to globi component

--- src/lib/components/globi_list.svelte
+++ src/lib/components/globi_list.svelte
@@ -13,8 +13,18 @@
           {#if taxon.target_scientific_name}<span class="text-gray-400"
               >({taxon.target_scientific_name})</span
             >{/if}
+          <a class="external-link" href="https://www.inaturalist.org/taxa/{taxon.target_taxon_id}"
+            >iNaturalist link</a
+          >
         </li>
       {/if}
     {/each}
   </ul>
 {/if}
+
+<style>
+  .external-link {
+    font-size: 0.85rem;
+    margin-left: 0.5rem;
+  }
+</style>
