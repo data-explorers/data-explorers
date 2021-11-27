747505c1ddc58eab41661a1863da1d48fb8ed3c8
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-11-27 03:28:25 -0600

    display both entire taxa list and indicator species fo Los Angeles

---
+++ src/routes/users/[users]/los-angeles-bioblitz/indicator-species.svelte
@@ -0,0 +1,65 @@
+<script context="module">
+  import data from '$lib/data/data.json';
+
+  export async function load({ page }) {
+    let page_parts = page.path.split('/');
+    let user = data.filter((user) => user.username == page_parts[2])[0];
+    let project = user.projects.filter((project) => project.slug == page_parts[3])[0];
+    return { props: { user, project } };
+  }
+</script>
+
+<script>
+  import ProjectHeader from '$lib/components/project_header.svelte';
+  import allTaxa from '$lib/data/los-angeles-bioblitz/taxa.json';
+
+  let taxa = allTaxa.filter((t) => !!t.taxon_group);
+
+  export let user;
+  export let project;
+</script>
+
+<ProjectHeader {project} {user} />
+
+<h1>Indicator Species</h1>
+
+<div class="grid lg:grid-cols-4 md:grid-cols-3  sm:grid-cols-2 justify-center gap-3">
+  {#each taxa as { image_url, common_name, count, scientific_name, user_login, taxon_group, taxon_id, type }}
+    <div class="card bordered">
+      <a href="/users/{user.username}/{project.slug}/taxa/{taxon_id}">
+        <figure>
+          <img src={image_url} alt="photo of {common_name}" />
+        </figure>
+      </a>
+
+      <div class="card-body">
+        <a href="/users/{user.username}/{project.slug}/taxa/{taxon_id}">
+          {#if common_name}<div class="text-lg font-medium">{common_name}</div>{/if}
+          {#if scientific_name}<div class="text-gray-400">({scientific_name})</div>{/if}
+          <div>{count} observations</div>
+          <div>{taxon_group}</div>
+          <div>{type}</div>
+        </a>
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  img {
+    height: 225px;
+    object-fit: cover;
+  }
+  @media (max-width: 640px) {
+    img {
+      height: 400px;
+      object-fit: cover;
+    }
+  }
+  .card.bordered {
+    border-radius: 0;
+  }
+  .card-body {
+    padding: 1rem;
+  }
+</style>
