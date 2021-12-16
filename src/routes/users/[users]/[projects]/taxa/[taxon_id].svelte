a51437de97ac37e2710fa1d41804e1eab6caa31e
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-12-16 11:43:45 -0600

    update taxa show to work with higher level taxa that were not observed

--- src/routes/users/[users]/[projects]/taxa/[taxon_id].svelte
+++ src/routes/users/[users]/[projects]/taxa/[taxon_id].svelte
@@ -8,7 +8,37 @@
   export async function load({ page }) {
     let taxaData = await import(`../../../../../lib/data/${page.params.projects}/taxa.json`);
     let taxa = taxaData.default;
-    let taxon = taxa.filter((taxon) => taxon.taxon_id == page.params.taxon_id)[0] || {};
+
+    // find taxon that has observations
+    let taxon = taxa.filter((taxon) => taxon.taxon_id == Number(page.params.taxon_id))[0];
+    if (!taxon) {
+      // find higher level taxon
+      let higherTaxon = taxa
+        .filter((t) => t.taxon_ids)
+        .filter((taxon) => taxon.taxon_ids.split('|').includes(page.params.taxon_id))[0];
+      if (higherTaxon) {
+        let names = higherTaxon.names.split('|');
+        let taxon_ids = higherTaxon.taxon_ids.split('|');
+        let common_names = higherTaxon.common_names.split('|');
+        let index = taxon_ids.indexOf(page.params.taxon_id);
+
+        taxon = {
+          scientific_name: names[index],
+          common_name: common_names[index],
+          taxon_id: Number(taxon_ids[index]),
+          image_url: higherTaxon['image_url'],
+          user_login: higherTaxon['user_login'],
+          count: null,
+          is_species: false,
+          taxon_ids: taxon_ids.slice(0, index + 1).join('|'),
+          names: names.slice(0, index + 1).join('|'),
+          common_names: common_names.slice(0, index + 1).join('|')
+        };
+      } else {
+        taxon = {};
+      }
+    }
+
     let observationData = await import(
       `../../../../../lib/data/${page.params.projects}/observations.json`
     );
@@ -19,7 +49,10 @@
 
     let user = data.filter((user) => user.username === page.params.users)[0];
     let project = user.projects.filter((project) => project.slug === page.params.projects)[0];
-    let interactions = allInteractions.filter((i) => i.subject_taxon_id == page.params.taxon_id);
+    let interactions = allInteractions
+      .filter((i) => i.subject_taxon_id)
+      .filter((i) => i.subject_taxon_id == page.params.taxon_id);
+
     let projectPath = `/users/${user.username}/${project.slug}`;
 
     return {
