e94fdca1d06cc0cf0fd81198f38daa45fbb41082
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-12-16 18:24:06 -0600

    update copy on about page

--- app/src/routes/about.svelte
+++ app/src/routes/about.svelte
@@ -1,14 +1,12 @@
 <div class="prose">
   <h1>About</h1>
-  <p>
-    Cillum et ex esse proident adipisicing magna reprehenderit minim. Enim proident et culpa ea
-    dolore veniam dolor sint fugiat. Reprehenderit fugiat consectetur qui magna esse mollit elit.
-  </p>
 
   <p>
-    In est consequat non commodo ipsum in exercitation ut eiusmod id magna ut officia. Quis velit
-    anim fugiat ad. Laborum excepteur aliquip fugiat quis magna occaecat. Nulla et nulla incididunt
-    tempor sit pariatur laboris cupidatat tempor excepteur ipsum nulla sit. Magna minim aliqua ipsum
-    veniam ullamco non aute nostrud qui irure.
+    iNaturalist Data Explorer is a website that allows iNaturalist project organizers to import
+    their iNaturalist data, upload education materials, and add pre-built interactive data
+    exploration tools for their project. By giving citizen scientists the chance to explore the data
+    that they help collect, we hope citizen scientists will be encouraged to form their own
+    questions, look for answers to their own questions, and thereby create a more engaging
+    experience for both participants and organizers.
   </p>
 </div>
