c1ef567742351363225080eb9062fb948c4c3c5f
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-12-18 07:48:32 -0600

    lint python files

--- notebooks/create_normalized_taxa.py
+++ notebooks/create_normalized_taxa.py
@@ -4,111 +4,123 @@ import pandas as pd
 def add_count_column(df, count_col):
     # count the number of taxon_id
     temp = df.copy()
-    temp['temp_count'] = 1
-    temp['temp_count'] = temp['temp_count'].astype(int)
+    temp["temp_count"] = 1
+    temp["temp_count"] = temp["temp_count"].astype(int)
 
-    # create a df with 2 columns: taxon_id and temp_count       
-    count_df = temp.groupby(['taxon_id'])['temp_count'].sum().reset_index()
+    # create a df with 2 columns: taxon_id and temp_count
+    count_df = temp.groupby(["taxon_id"])["temp_count"].sum().reset_index()
     count_df.rename(columns={"temp_count": count_col}, inplace=True)
 
     df = df.merge(count_df)
     return df
 
-def append_df(df):  
-    #  try to match the species count that is shown on inaturalist project page     
-    df['is_species'] = df['species'].notna()
-    adjust_is_species_for_higher_ranks(df)  
-        
+
+def append_df(df):
+    #  try to match the species count that is shown on inaturalist project page
+    df["is_species"] = df["species"].notna()
+    adjust_is_species_for_higher_ranks(df)
+
     # count the number of observations for a taxon_id
-    df = add_count_column(df, 'observations_count')
+    df = add_count_column(df, "observations_count")
     return df
 
-    
+
 def add_row(row, rank, index):
     temp = {}
-    temp['id'] = row['id']
-    temp['taxon_id'] = row[rank + '_id']
-    temp['common_name'] = row[rank + '_common_name']
-    temp['scientific_name'] = row[rank]
-    
-    if pd.notna(row['user_login']):
-        temp['user_login'] = row['user_login']
-        temp['image_url'] = row['image_url']
+    temp["id"] = row["id"]
+    temp["taxon_id"] = row[rank + "_id"]
+    temp["common_name"] = row[rank + "_common_name"]
+    temp["scientific_name"] = row[rank]
+
+    if pd.notna(row["user_login"]):
+        temp["user_login"] = row["user_login"]
+        temp["image_url"] = row["image_url"]
     else:
-        temp['user_login'] = row[rank + '_photo_attribution']
-        temp['image_url'] = row[rank + '_photo_url']
-        
-    temp['rank'] = rank
-    temp['taxon_ids'] = ('|').join(row['taxon_ids'].split('|')[0: index +1])
-    temp['common_names'] = ('|').join(row['common_names'].split('|')[0: index+1])
-    temp['scientific_names'] = ('|').join(row['scientific_names'].split('|')[0: index+1])
+        temp["user_login"] = row[rank + "_photo_attribution"]
+        temp["image_url"] = row[rank + "_photo_url"]
+
+    temp["rank"] = rank
+    temp["taxon_ids"] = ("|").join(row["taxon_ids"].split("|")[0 : index + 1])
+    temp["common_names"] = ("|").join(row["common_names"].split("|")[0 : index + 1])
+    temp["scientific_names"] = ("|").join(
+        row["scientific_names"].split("|")[0 : index + 1]
+    )
     return temp
+
 
 def add_nonmain_row(row):
     temp = {}
     cols = [
-        'id', 'taxon_id', 'common_name', 'scientific_name',
-        'user_login', 'image_url', 'rank',
-        'taxon_ids', 'common_names', 'scientific_names'
+        "id",
+        "taxon_id",
+        "common_name",
+        "scientific_name",
+        "user_login",
+        "image_url",
+        "rank",
+        "taxon_ids",
+        "common_names",
+        "scientific_names",
     ]
     for col in cols:
-        temp[col] = row[col]            
+        temp[col] = row[col]
     return temp
+
 
 def create_taxa_df(df):
     # create a new df with rows for each taxa and eac higher taxa
     new_rows = []
-    ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
-    
+    ranks = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
+
     for index, row in df.iterrows():
         for index, rank in enumerate(ranks):
             if pd.isna(row[rank]):
                 continue
-                
+
             temp = add_row(row, rank, index)
             new_rows.append(temp)
-        
-        # handle taxa that not one of the main ranks       
-        if row['taxon_id'] not in [row[rank + '_id'] for rank in ranks]:
+
+        # handle taxa that not one of the main ranks
+        if row["taxon_id"] not in [row[rank + "_id"] for rank in ranks]:
             temp = add_nonmain_row(row)
             new_rows.append(temp)
-            
-            
-    new_df =  pd.DataFrame(new_rows) 
-    
-    temp = df[['is_species', 'observations_count', 'taxon_id']].drop_duplicates()
-    new_df = new_df.merge(temp, how='left')
-    
-    new_df.loc[new_df['observations_count'].isna(), 'observations_count'] = 0
-    new_df['observations_count'] = new_df['observations_count'].astype(int)
-     
-    # count the total number of occurences for a taxa including higher taxa
-    new_df = add_count_column(new_df, 'taxa_count')
 
-    # sort newest observations first so that we get use the photo for the newest observations
-    new_df = new_df.sort_values(['id'], ascending=False)
-    new_df = new_df.drop_duplicates(subset=['taxon_id'])
-    new_df = new_df.sort_values(['observations_count'], ascending=False)
-    
+    new_df = pd.DataFrame(new_rows)
+
+    temp = df[["is_species", "observations_count", "taxon_id"]].drop_duplicates()
+    new_df = new_df.merge(temp, how="left")
+
+    new_df.loc[new_df["observations_count"].isna(), "observations_count"] = 0
+    new_df["observations_count"] = new_df["observations_count"].astype(int)
+
+    # count the total number of occurences for a taxa including higher taxa
+    new_df = add_count_column(new_df, "taxa_count")
+
+    # sort newest observations first so that we get use the photo for the
+    # newest observations
+    new_df = new_df.sort_values(["id"], ascending=False)
+    new_df = new_df.drop_duplicates(subset=["taxon_id"])
+    new_df = new_df.sort_values(["observations_count"], ascending=False)
+
     return new_df
 
 
-
-# try to match the species count that is shown on inaturalist project page   
+# try to match the species count that is shown on inaturalist project page
 # if a rank higher than species is the lowest occurence of the taxa, it is treated as
-# a species. e.g. if there are no species for genus AA, genus AA 'is_species' is True 
+# a species. e.g. if there are no species for genus AA, genus AA 'is_species' is True
 def adjust_is_species_for_higher_ranks(df):
-    adjust_is_species_for_rank(df, 'genus')    
-    adjust_is_species_for_rank(df, 'family')   
-    adjust_is_species_for_rank(df, 'order')  
-    adjust_is_species_for_rank(df, 'class')  
-    adjust_is_species_for_rank(df, 'phylum')    
-    adjust_is_species_for_rank(df, 'kingdom') 
-    df.loc[df['scientific_name'].str.contains(' × ') == True, 'is_species'] = True
+    adjust_is_species_for_rank(df, "genus")
+    adjust_is_species_for_rank(df, "family")
+    adjust_is_species_for_rank(df, "order")
+    adjust_is_species_for_rank(df, "class")
+    adjust_is_species_for_rank(df, "phylum")
+    adjust_is_species_for_rank(df, "kingdom")
+    df.loc[df["scientific_name"].str.contains(" × ") == True, "is_species"] = True
+
 
 def adjust_is_species_for_rank(df, rank):
     tmp = df.copy()
-    taxa = list(df[df['is_species'] == True][rank].unique())
-    tmp = tmp[(tmp['is_species'] == False) & (tmp[rank].notna())]
-    for index, row in tmp[~ tmp[rank].isin(taxa)].iterrows():
-        df.at[index, 'is_species'] = True
+    taxa = list(df[df["is_species"] == True][rank].unique())
+    tmp = tmp[(tmp["is_species"] == False) & (tmp[rank].notna())]
+    for index, row in tmp[~tmp[rank].isin(taxa)].iterrows():
+        df.at[index, "is_species"] = True
--- notebooks/process_files.py
+++ notebooks/process_files.py
@@ -1,116 +1,137 @@
-ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
+ranks = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
 
 
 def add_rank_data(obj, rank, taxon):
-    obj[f"{rank}_id"] = str(taxon['id'])
-    if 'wikipedia_url' in taxon and taxon['wikipedia_url']:
-        obj[f"{rank}_wikipedia_url"] = taxon['wikipedia_url']
+    obj[f"{rank}_id"] = str(taxon["id"])
+    if "wikipedia_url" in taxon and taxon["wikipedia_url"]:
+        obj[f"{rank}_wikipedia_url"] = taxon["wikipedia_url"]
     else:
-        obj[f"{rank}_wikipedia_url"] = ''
-        
-        
-    if 'iconic_taxon_name' in taxon and taxon['iconic_taxon_name']:
-        obj[f"{rank}_iconic_taxon_name"] = taxon['iconic_taxon_name']
+        obj[f"{rank}_wikipedia_url"] = ""
+
+    if "iconic_taxon_name" in taxon and taxon["iconic_taxon_name"]:
+        obj[f"{rank}_iconic_taxon_name"] = taxon["iconic_taxon_name"]
     else:
-        obj[f"{rank}_iconic_taxon_name"] = ''       
-    
-    if 'preferred_common_name' in taxon and taxon['preferred_common_name']:
-        obj[f"{rank}_common_name"] = taxon['preferred_common_name']
+        obj[f"{rank}_iconic_taxon_name"] = ""
+
+    if "preferred_common_name" in taxon and taxon["preferred_common_name"]:
+        obj[f"{rank}_common_name"] = taxon["preferred_common_name"]
     else:
-        obj[f"{rank}_common_name"] = '' 
-        
-        
-    obj[rank] = taxon['name']
-        
+        obj[f"{rank}_common_name"] = ""
+
+    obj[rank] = taxon["name"]
+
     add_default_photo(obj, taxon, rank)
-    
+
 
 def add_default_photo(obj, taxon, rank=False):
-    if 'default_photo' in taxon and taxon['default_photo']:
-        photo = taxon['default_photo']
-        
-        field = f'{rank}_photo_url'  if rank else 'photo_url'
-        if 'medium_url' in photo and photo['medium_url']:
-            obj[field] = photo['medium_url']
-        else:
-            obj[field] = photo['original_url']
-            
-        field = f'{rank}_photo_attribution'  if rank else 'photo_attribution'
-        if 'attribution' in photo and photo['attribution']:
-            obj[field] = photo['attribution']
-        else:
-             obj[field] = ''
-            
-        
-        field = f'{rank}_photo_license_code'  if rank else 'photo_license_code'
-        if 'license_code' in photo and photo['license_code']:
-            obj[field] = photo['license_code']
-        else:
-            obj[field] = ''
+    if "default_photo" in taxon and taxon["default_photo"]:
+        photo = taxon["default_photo"]
 
-            
+        field = f"{rank}_photo_url" if rank else "photo_url"
+        if "medium_url" in photo and photo["medium_url"]:
+            obj[field] = photo["medium_url"]
+        else:
+            obj[field] = photo["original_url"]
+
+        field = f"{rank}_photo_attribution" if rank else "photo_attribution"
+        if "attribution" in photo and photo["attribution"]:
+            obj[field] = photo["attribution"]
+        else:
+            obj[field] = ""
+
+        field = f"{rank}_photo_license_code" if rank else "photo_license_code"
+        if "license_code" in photo and photo["license_code"]:
+            obj[field] = photo["license_code"]
+        else:
+            obj[field] = ""
+
+
 def process_inat_data(json_data):
-            
+
     result = {}
-    if 'rank' in json_data and json_data['rank']:
-        result['rank'] = json_data['rank']
-        add_rank_data(result, json_data['rank'], json_data)
+    if "rank" in json_data and json_data["rank"]:
+        result["rank"] = json_data["rank"]
+        add_rank_data(result, json_data["rank"], json_data)
 
-    for taxon in json_data['ancestors']:
-        if taxon['rank'] == 'kingdom':
-            add_rank_data(result, 'kingdom', taxon)
-        elif taxon['rank'] == 'phylum':
-            add_rank_data(result, 'phylum', taxon)
-        elif taxon['rank'] == 'class':
-            add_rank_data(result, 'class', taxon)
-        elif taxon['rank'] == 'order':
-            add_rank_data(result, 'order', taxon)
-        elif taxon['rank'] == 'family':
-            add_rank_data(result, 'family', taxon)
-        elif taxon['rank'] == 'genus':
-            add_rank_data(result, 'genus', taxon)
-        elif taxon['rank'] == 'species':
-            add_rank_data(result, 'species', taxon)
+    for taxon in json_data["ancestors"]:
+        if taxon["rank"] == "kingdom":
+            add_rank_data(result, "kingdom", taxon)
+        elif taxon["rank"] == "phylum":
+            add_rank_data(result, "phylum", taxon)
+        elif taxon["rank"] == "class":
+            add_rank_data(result, "class", taxon)
+        elif taxon["rank"] == "order":
+            add_rank_data(result, "order", taxon)
+        elif taxon["rank"] == "family":
+            add_rank_data(result, "family", taxon)
+        elif taxon["rank"] == "genus":
+            add_rank_data(result, "genus", taxon)
+        elif taxon["rank"] == "species":
+            add_rank_data(result, "species", taxon)
 
-    if 'ancestor_ids' in json_data and json_data['ancestor_ids']:
-        result['ancestor_ids'] = ','.join([str(id) for id in json_data['ancestor_ids']])
-        
+    if "ancestor_ids" in json_data and json_data["ancestor_ids"]:
+        result["ancestor_ids"] = ",".join([str(id) for id in json_data["ancestor_ids"]])
+
     return result
 
-           
+
 def add_concatenated_columns(inat_df):
-    inat_df['taxon_ids'] = (inat_df['kingdom_id'] 
-                            + '|' + inat_df['phylum_id'] 
-                            + '|' + inat_df['class_id']
-                            + '|' + inat_df['order_id']
-                            + '|' + inat_df['family_id']
-                            + '|' + inat_df['genus_id']
-                            + '|' + inat_df['species_id'])
-
-    inat_df['scientific_names'] = (inat_df['kingdom'] 
-                            + '|' + inat_df['phylum'] 
-                            + '|' + inat_df['class']
-                            + '|' + inat_df['order']
-                            + '|' + inat_df['family']
-                            + '|' + inat_df['genus']
-                            + '|' + inat_df['species'])
-
-    inat_df['common_names'] = (inat_df['kingdom_common_name'] 
-                            + '|' + inat_df['phylum_common_name'] 
-                            + '|' + inat_df['class_common_name']
-                            + '|' + inat_df['order_common_name']
-                            + '|' + inat_df['family_common_name']
-                            + '|' + inat_df['genus_common_name']
-                            + '|' + inat_df['species_common_name'])
-    
-    inat_df.loc[inat_df['rank'].isin(ranks) == False, 'common_names'] = (
-        inat_df['common_names'] + '|' + inat_df['common_name']
+    inat_df["taxon_ids"] = (
+        inat_df["kingdom_id"]
+        + "|"
+        + inat_df["phylum_id"]
+        + "|"
+        + inat_df["class_id"]
+        + "|"
+        + inat_df["order_id"]
+        + "|"
+        + inat_df["family_id"]
+        + "|"
+        + inat_df["genus_id"]
+        + "|"
+        + inat_df["species_id"]
     )
-    
-    inat_df.loc[inat_df['rank'].isin(ranks) == False, 'scientific_names'] = (
-        inat_df['scientific_names'] + '|' + inat_df['scientific_name']
+
+    inat_df["scientific_names"] = (
+        inat_df["kingdom"]
+        + "|"
+        + inat_df["phylum"]
+        + "|"
+        + inat_df["class"]
+        + "|"
+        + inat_df["order"]
+        + "|"
+        + inat_df["family"]
+        + "|"
+        + inat_df["genus"]
+        + "|"
+        + inat_df["species"]
     )
-    
-    inat_df.loc[inat_df['rank'].isin(ranks) == False, 'taxon_ids'] = (
-        inat_df['taxon_ids'] + '|' + inat_df['taxon_id']
+
+    inat_df["common_names"] = (
+        inat_df["kingdom_common_name"]
+        + "|"
+        + inat_df["phylum_common_name"]
+        + "|"
+        + inat_df["class_common_name"]
+        + "|"
+        + inat_df["order_common_name"]
+        + "|"
+        + inat_df["family_common_name"]
+        + "|"
+        + inat_df["genus_common_name"]
+        + "|"
+        + inat_df["species_common_name"]
+    )
+
+    inat_df.loc[inat_df["rank"].isin(ranks) == False, "common_names"] = (
+        inat_df["common_names"] + "|" + inat_df["common_name"]
+    )
+
+    inat_df.loc[inat_df["rank"].isin(ranks) == False, "scientific_names"] = (
+        inat_df["scientific_names"] + "|" + inat_df["scientific_name"]
+    )
+
+    inat_df.loc[inat_df["rank"].isin(ranks) == False, "taxon_ids"] = (
+        inat_df["taxon_ids"] + "|" + inat_df["taxon_id"]
     )
