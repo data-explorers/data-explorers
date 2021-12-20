96802847dc81ecbc38ccd1bd10fbe15ffe919977
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-12-20 03:23:17 -0600

    refactor how to create combined taxa and project taxa files

--- notebooks/create_normalized_taxa.py
+++ notebooks/create_normalized_taxa.py
@@ -1,4 +1,43 @@
 import pandas as pd
+import numpy as np
+
+main_ranks = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
+
+# https://en.wikipedia.org/wiki/Taxonomic_rank
+all_ranks = [
+    "stateofmatter",
+    "kingdom",
+    "phylum",
+    "subphylum",
+    "class",
+    "subclass",
+    "infraclass",
+    "subterclass",
+    "superorder",
+    "order",
+    "suborder",
+    "infraorder",
+    "zoosection",
+    "zoosubsection",
+    "superfamily",
+    "epifamily",
+    "family",
+    "subfamily",
+    "tribe",
+    "subtribe",
+    "genus",
+    "subgenus",
+    "section",
+    "complex",
+    "species",
+    "subspecies",
+    "variety",
+    "hybrid",
+]
+
+# =====================
+# create taxa file for each project
+# =====================
 
 
 def add_count_column(df, count_col):
@@ -14,90 +53,118 @@ def add_count_column(df, count_col):
     df = df.merge(count_df)
     return df
 
-
-def append_df(df):
-    # count the number of observations for a taxon_id
-    df = add_count_column(df, "observations_count")
-    return df
-
-
-def add_row(row, rank, index):
-    temp = {}
-    temp["id"] = row["id"]
-    temp["taxon_id"] = row[rank + "_id"]
-    temp["common_name"] = row[rank + "_common_name"]
-    temp["scientific_name"] = row[rank]
-
-    if pd.notna(row["user_login"]):
-        temp["user_login"] = row["user_login"]
-        temp["image_url"] = row["image_url"]
-    else:
-        temp["user_login"] = row[rank + "_photo_attribution"]
-        temp["image_url"] = row[rank + "_photo_url"]
-
-    temp["rank"] = rank
-    temp["parent_id"] = row["parent_id"]
-    temp["taxon_ids"] = ("|").join(row["taxon_ids"].split("|")[0 : index + 1])
-    temp["common_names"] = ("|").join(row["common_names"].split("|")[0 : index + 1])
-    temp["scientific_names"] = ("|").join(
-        row["scientific_names"].split("|")[0 : index + 1]
-    )
-    return temp
-
-
-def add_nonmain_row(row):
-    temp = {}
-    cols = [
-        "id",
-        "taxon_id",
-        "common_name",
-        "scientific_name",
-        "user_login",
-        "image_url",
-        "rank",
-        "parent_id",
-        "taxon_ids",
-        "common_names",
-        "scientific_names",
-    ]
-    for col in cols:
-        temp[col] = row[col]
-    return temp
-
-
-def create_taxa_df(df):
-    # create a new df with rows for each taxa and eac higher taxa
+def create_taxa_base_df(df, append_taxon_method):
+    # create records for the observed taxon and derived higher levels
     new_rows = []
-    ranks = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
-
+    # create taxa records for each obersavations
     for index, row in df.iterrows():
-        for index, rank in enumerate(ranks):
-            if pd.isna(row[rank]):
-                continue
+        # make sure the taxa ranks are in the correct order
+        row_ranks = get_row_ranks(row, all_ranks, "ancestor_ids", "taxon_id")
+        # limit the ranks to the main ranks and rank of the observation
+        allowed_ranks = main_ranks + [row["rank"]]
+        rank_history = []
 
-            temp = add_row(row, rank, index)
-            new_rows.append(temp)
+        for rank in row_ranks:
+            if rank in allowed_ranks:
+                rank_history.append(rank)
+                taxon = create_taxon(row, rank, rank_history)
+                append_taxon_method(taxon, row, rank)
 
-        # handle taxa that not one of the main ranks
-        if row["taxon_id"] not in [row[rank + "_id"] for rank in ranks]:
-            temp = add_nonmain_row(row)
-            new_rows.append(temp)
+                new_rows.append(taxon)
 
     new_df = pd.DataFrame(new_rows)
-
-    temp = df[["observations_count", "taxon_id"]].drop_duplicates()
-    new_df = new_df.merge(temp, how="left")
-
-    new_df.loc[new_df["observations_count"].isna(), "observations_count"] = 0
-    new_df["observations_count"] = new_df["observations_count"].astype(int)
-
     # count the total number of occurences for a taxa including higher taxa
     new_df = add_count_column(new_df, "taxa_count")
 
-    # sort newest observations first so that we get use the photo for the
-    # newest observations
-    new_df = new_df.sort_values(["id"], ascending=False)
+    # sort taxa records by observations_count and id so we can use photos
+    # from the newest observations.
+    new_df = new_df.sort_values(['taxon_id', 'observations_count', "id"], ascending=False)
     new_df = new_df.drop_duplicates(subset=["taxon_id"])
     new_df = new_df.sort_values(["observations_count"], ascending=False)
 
     return new_df
+
+
+def append_taxon(taxon, row, rank):
+    taxon["id"] = row["id"]
+    taxon["user_login"] = row["user_login"]
+    taxon["image_url"] = row["image_url"]
+
+    if rank == row["rank"]:
+        taxon["observations_count"] = row["observations_count"]
+    else:
+        taxon["observations_count"] = 0
+
+
+def create_taxa_df(df):
+    return create_taxa_base_df(df, append_taxon)
+
+
+def append_taxon_la(taxon, row, rank):
+    taxon["id"] = row["id"]
+    taxon["user_login"] = row["user_login"]
+    taxon["image_url"] = row["image_url"]
+
+    if rank == row["rank"]:
+        taxon["observations_count"] = row["observations_count"]
+        taxon["taxon_group"] = row["taxon_group"]
+        taxon["type"] = row["type"]
+    else:
+        taxon["observations_count"] = 0
+        taxon["taxon_group"] = np.nan
+        taxon["type"] = np.nan
+
+
+def create_taxa_la_df(df):
+    return create_taxa_base_df(df, append_taxon_la)
+
+
+# =====================
+# create taxa file all projects
+# =====================
+
+
+def get_row_ranks(row, all_ranks, ancestor_col, id_col):
+    # use ancestor_ids to set the order of the ranks
+    ids = row[ancestor_col].split("|")
+    ids.append(row[id_col])
+
+    temp = {}
+    for rank in all_ranks:
+        if rank in row and pd.notna(row[rank]):
+            temp[ids.index(row[rank + "_id"])] = rank
+
+    # sort the ranks
+    return [val for key, val in sorted(temp.items(), key=lambda ele: ele[0])]
+
+
+def create_taxon(row, rank, rank_history):
+    taxon = {}
+    taxon["taxon_id"] = row[rank + "_id"]
+    taxon["common_name"] = row[rank + "_common_name"]
+    taxon["scientific_name"] = row[rank]
+    taxon["rank"] = rank
+    # since we don't use all the iNaturalist ranks, use the previous rank
+    # instead of the parent_id from iNaturalist to get parent_id
+    if len(rank_history) > 1:
+        taxon["parent_id"] = row[rank_history[-2] + "_id"]
+    # use iNaturalist parent_id if there is only one rank
+    else:
+        taxon["parent_id"] = row[rank + "_parent_id"]
+
+    ids = []
+    names = []
+    common_names = []
+    for rank in rank_history:
+        ids.append(row[rank + "_id"])
+        names.append(row[rank])
+        common_name = (
+            row[rank + "_common_name"] if pd.notna(row[rank + "_common_name"]) else ''
+        )
+        common_names.append(common_name)
+
+    taxon["taxon_ids"] = "|".join(ids)
+    taxon["common_names"] = "|".join(common_names)
+    taxon["scientific_names"] = "|".join(names)
+
+    return taxon
