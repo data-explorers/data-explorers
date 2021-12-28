89044068370e735184b683bad4c2587f84d57b40
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-12-27 19:01:52 -0600

    fix - exclude current taxon from observed species

--- app/src/lib/components/taxa_observed_species.svelte
+++ app/src/lib/components/taxa_observed_species.svelte
@@ -21,6 +21,7 @@
   function getObservedSpecies(taxa, taxon) {
     return taxa
       .filter((t) => speciesRanks.includes(t.rank))
+      .filter((t) => t.taxon_id !== taxon.taxon_id)
       .filter((t) => t.taxon_ids.split('|').includes('' + taxon.taxon_id));
   }
 
