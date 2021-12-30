c39f0d1a4b2bf3da35cedb75a93ef361dda0e3e0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-12-30 05:04:40 -0600

    replace <Popup> with <MyPopup> for map_popup_observation.svelte

--- app/src/lib/components/map_popup_observation.svelte
+++ app/src/lib/components/map_popup_observation.svelte
@@ -1,26 +1,28 @@
 <script>
   import { formatTaxonDisplayName } from '$lib/formatUtils';
-  import { Popup } from 'svelte-leafletjs';
   import ObservationData from '$lib/components/observation_data.svelte';
+  import { MyPopup } from '$lib/vendor/svelte-leaflet';
 
   export let observation;
   export let projectPath;
 </script>
 
-<Popup>
-  {#if observation.image_url}
-    <img
-      class="mb-2"
-      src={observation.image_url.replace('medium', 'small')}
-      alt="photo of {formatTaxonDisplayName(observation)}"
+<MyPopup>
+  {#if observation}
+    {#if observation.image_url}
+      <img
+        class="mb-2"
+        src={observation.image_url.replace('medium', 'small')}
+        alt="photo of {formatTaxonDisplayName(observation)}"
+      />
+    {:else}
+      <img class="mb-2" src="/images/missing-image.png" alt="" />
+    {/if}
+    <ObservationData
+      {observation}
+      {projectPath}
+      compactLayout={true}
+      enableZoomToObservation={false}
     />
-  {:else}
-    <img class="mb-2" src="/images/missing-image.png" alt="" />
   {/if}
-  <ObservationData
-    {observation}
-    {projectPath}
-    compactLayout={true}
-    enableZoomToObservation={false}
-  />
-</Popup>
+</MyPopup>
