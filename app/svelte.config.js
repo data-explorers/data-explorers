5ca90a958587c35feae55c4346f2ec7b6b8dd92a
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-01-10 10:59:54 -0600

    update config files after upgrading packages

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -8,25 +8,16 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
+    adapter: adapter(),
     paths: {
+      // /inaturalist_data_explorer
       base: dev ? '' : '/inaturalist_data_explorer'
     },
     vite: {
       plugins: [dsv()]
     }
   },
-
-  preprocess: [
-    preprocess({
-      postcss: true
-    })
-  ]
+  preprocess: [preprocess({})]
 };
 
 export default config;
