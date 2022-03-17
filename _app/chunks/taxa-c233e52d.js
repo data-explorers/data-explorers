b70222995fdd49e6ff6788dd3627fc723ac8f0bf
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-03-17 11:22:36 -0500

    Updates

---
+++ _app/chunks/taxa-c233e52d.js
@@ -0,0 +1,6 @@
+var e=`## Why are there so many pictures of sand and snow, and so few pictures of animals or plants?
+
+Much of our iNaturalist data comes from volunteers on our wildlife tracking project, the Cedar Creek Wildlife Survey. On this project, community members and trained wildlife trackers walk the reserve\u2019s internal roads in small groups and identify the species that they see through footprints, trails, scat and other things animals leave behind. These things are much easier to see and study than the live wild animals, which are often skittish and afraid of humans!
+
+As we grow our iNaturalist user community, we will continue to supplement these track and sign photos with additional photos of live animals, live plants and other living organisms. You can help with this effort by documenting what _you_ see on your next visit to Cedar Creek!
+`;export{e as default};
